9ca0ff80405dc8339ab2000ba5efcd34bec0f434
Anthony <astabile.design@gmail.com>
2018-09-22 16:34:48 -0500

    added exec command

--- inputs.js
+++ inputs.js
@@ -2,8 +2,8 @@
 const { readdirSync } = require('fs')
 
 // INPUTS
-const inputs = readdirSync(`${__dirname}/actions`)
-  .map(filename => `./actions/${filename}`)
-  .map(filepath => require(filepath))
+const inputs = readdirSync(`${__dirname}/actions`) 
+  .map(filename => `./actions/${filename}`) 
+  .map(filepath => require(filepath)) 
 
 module.exports = { inputs }
